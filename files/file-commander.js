4cff5eb88fd27c7215b2c35770cc52e7c2e3bb67
SirNando <matiasnandin@hotmail.com>
2024-02-09 12:58:18 -0300

    Added remaining remove, rename, add functions for file commander

--- files/file-commander.js
+++ files/file-commander.js
@@ -19,8 +19,52 @@ const { Buffer } = require("buffer");
     }
   }
 
+  async function deleteFile(path) {
+    try {
+      await fs.unlink(path);
+      return console.log(`File at path ${path} was deleted`);
+    } catch (error) {
+      // file does not exist
+      return console.log(`File ${path} was not found.`);
+    }
+  }
+
+  async function renameFile(path, newPath) {
+    try {
+      await fs.rename(path, newPath);
+      return console.log(`Renamed file ${path} to ${newPath}`);
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        return console.log(
+          "No file at this path to rename, or the destination does not exist"
+        );
+      } else {
+        return console.log(
+          `An error occurred when trying to remove the file: ${error}`
+        );
+      }
+    }
+  }
+
+  async function addToFile(path, text) {
+    try {
+      await fs.appendFile(path, text);
+      return console.log(`Added ${text} to file ${path}`);
+    } catch (error) {
+      return console.log(`File ${path} was not found`);
+    }
+  }
+
+  // helpers
+  function getArguments(string, startingPos) {
+    return string.substring(startingPos.length + 1);
+  }
+
   // commands
   const CREATE_FILE = "create a file";
+  const DELETE_FILE = "delete the file";
+  const RENAME_FILE = "rename the file";
+  const ADD_TO_FILE = "add to the file";
 
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -41,15 +85,29 @@ const { Buffer } = require("buffer");
     // decode buffer to get command
     const command = buff.toString("utf-8");
 
-    // create a file <path>
     if (command.startsWith(CREATE_FILE)) {
-      const filePath = command.substring(CREATE_FILE.length + 1);
-      console.log(filePath);
+      // create a file <path>
+      const filePath = getArguments(command, CREATE_FILE);
       createFile(filePath);
+    } else if (command.startsWith(DELETE_FILE)) {
+      // delete the file <path>
+      const filePath = getArguments(command, DELETE_FILE);
+      deleteFile(filePath);
+    } else if (command.startsWith(RENAME_FILE)) {
+      // rename the file <path> <new_path>
+      const [path, newPath] = getArguments(command, RENAME_FILE).split(" ");
+      renameFile(path, newPath);
+    } else if (command.startsWith(ADD_TO_FILE)) {
+      // add to the file <path> <text>
+      const arguments = getArguments(command, ADD_TO_FILE);
+      const path = arguments.split(" ")[0];
+      const text = arguments.substring(path.length + 1);
+      addToFile(path, text);
     }
   });
 
   const watcher = fs.watch("./command.txt");
+
   for await (const event of watcher) {
     if (event.eventType === "change") {
       commandFileHandler.emit("change");
