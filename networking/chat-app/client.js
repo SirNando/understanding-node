454ac7be213c3fe4c8cca1c1ec6f5d0833cf303d
SirNando <matiasnandin@hotmail.com>
2024-02-28 11:06:52 -0300

    Improved chat application by adding username and ID handling, along with a better UI

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { moveCursor } = require("readline");
 const readline = require("readline/promises");
 
 const rl = readline.createInterface({
@@ -6,20 +7,55 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function moveLine(dx, dy) {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+}
+
+function clearLine(dir) {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+}
+
 const socket = net.createConnection(
   { host: "127.0.0.1", port: 3008 },
   async () => {
     console.log("Connected to the server.");
+    let id;
 
-    const message = await rl.question("Enter a message:");
+    async function askMessage(question) {
+      const message = await rl.question(question);
 
-    socket.write(message);
+      // clear the line where the cursor is
+      await moveLine(0, -1);
+      await clearLine(0);
+      socket.write(`${id}-message-${message}`);
+    }
+    askMessage("Enter your name:");
+
+    socket.on("data", async (data) => {
+      console.log();
+      await moveLine(0, -1);
+      await clearLine(0);
+
+      if (data.toString("utf-8").substring(0, 2) === "id") {
+        // when we're getting the ID...
+        id = data.toString("utf-8").substring(3);
+        console.log(`Your ID is ${id}\n`);
+      } else {
+        console.log(data.toString("utf-8"));
+      }
+
+      askMessage("Enter a message:");
+    });
   }
 );
-
-socket.on("data", (data) => {
-  console.log(data.toString("utf-8"));
-});
 
 socket.on("end", () => {
   console.log("Connection was ended");
--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -4,16 +4,43 @@ const server = net.createServer();
 const clients = [];
 
 server.on("connection", (socket) => {
-  console.log("A connection was made to the server");
+  console.log(`A connection was made`);
   clients.push(socket);
 
-  socket.on("data", (data) => {
-    for (const client of clients) {
-      client.write(data);
+  function sendToAll(data) {
+    clients.map((s) => {
+      s.write(`${socket.username}: ${data}`);
+    });
+  }
+
+  function decodeBuffer(data, option = "message") {
+    const dataString = data.toString("utf-8");
+
+    switch (option) {
+      case "id": {
+        return dataString.substring(0, dataString.indexOf("-"));
+      }
+      case "message": {
+        return dataString.substring(dataString.indexOf("-message-") + 9);
+      }
     }
+  }
+
+  const id = clients.length;
+  socket.write(`id-${clients.length}`);
+  socket.id = id;
+
+  socket.on("data", (data) => {
+    if (!socket.username) {
+      socket.username = decodeBuffer(data, "message");
+      return sendToAll("*joined chat*");
+    }
+    sendToAll(decodeBuffer(data, "message"));
   });
 
-  
+  socket.on("error", () => {
+    sendToAll("*left chat*");
+  });
 });
 
 server.listen(3008, "127.0.0.1", () => {
