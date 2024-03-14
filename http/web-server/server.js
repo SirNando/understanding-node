e4a85ce46a994aaa45ff51c5f15aae4be30578e5
Matias Nandin <matiasnandin@hotmail.com>
2024-03-14 16:54:36 -0300

    Added file upload, css and js request handling to http server

--- http/web-server/server.js
+++ http/web-server/server.js
@@ -12,6 +12,60 @@ server.on("request", async (req, res) => {
 
     fileStream.pipe(res);
   }
+
+  if (req.url === "/styles.css" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/css");
+
+    const fileHandle = await fs.open("./public/styles.css", "r");
+    const fileStream = fileHandle.createReadStream();
+
+    fileStream.pipe(res);
+  }
+
+  if (req.url === "/script.js" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/javascript");
+
+    const fileHandle = await fs.open("./public/script.js", "r");
+    const fileStream = fileHandle.createReadStream();
+
+    fileStream.pipe(res);
+  }
+
+  if (req.url === "/login" && req.method === "POST") {
+    res.setHeader("Content-Type", "application/json");
+
+    res.statusCode = 200;
+    const body = {
+      message: "Logging you in...",
+    };
+
+    res.end(JSON.stringify(body));
+  }
+
+  if (req.url === "/user" && req.method === "PUT") {
+    res.setHeader("Content-Type", "application/json");
+
+    res.statusCode = 401;
+    const body = {
+      message: "You first need to log in",
+    };
+
+    res.end(JSON.stringify(body));
+  }
+
+  // upload route
+  if (req.url === "/upload" && req.method === "PUT") {
+    res.setHeader("Content-Type", "application/json");
+
+    const fileHandle = await fs.open("./storage/image.png", "w");
+    const fileStream = fileHandle.createWriteStream();
+
+    req.pipe(fileStream);
+
+    req.on("end", () => {
+      res.end(JSON.stringify({ message: "File uploaded" }));
+    });
+  }
 });
 
 server.listen(4080, "127.0.0.1", () => {
