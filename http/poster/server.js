f1486e1b07b6c75841cb86bb274d025f2a280242
SirNando <matiasnandin@hotmail.com>
2024-04-01 15:45:04 -0300

    Added handling for new routes in http project

--- http/poster/server.js
+++ http/poster/server.js
@@ -1,5 +1,8 @@
 const Pewter = require("../pewter");
 
+// { userId: 1, token: 234342 }
+const SESSIONS = [];
+
 const USERS = [
   { id: 1, name: "Liam Brown", username: "liam23", password: "string" },
   { id: 2, name: "Meredith Green", username: "merit.sky", password: "string" },
@@ -18,6 +21,60 @@ const POSTS = [
 const PORT = 8000;
 
 const server = new Pewter();
+
+// ------ Middleware ------ //
+server.beforeEach((req, res, next) => {
+  const routesToAuthenticate = [
+    "GET /api/user",
+    "PUT /api/user",
+    "POS /api/posts",
+    "DELETE /api/logout",
+  ];
+
+  if (routesToAuthenticate.indexOf(req.method + " " + req.url) !== -1) {
+    // if we have a token cookie, save the userId to the req object
+    if (req.headers.cookie) {
+      const token = req.headers.cookie?.split("=")[1];
+      console.log(token);
+      const session = SESSIONS.find((session) => session.token === token);
+      console.log(session);
+      if (session) {
+        console.log("Setting userId...");
+        req.userId = session.userId;
+        return next();
+      }
+    }
+    return res.status(401).json({ message: "Unauthorized" });
+  } else {
+    next();
+  }
+});
+
+// parsing JSON body (only if size of body is smaller than the size of the highwatermark)
+server.beforeEach((req, res, next) => {
+  if (req.headers["content-type"] === "application/json") {
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+    });
+    req.on("end", () => {
+      req.body = JSON.parse(body);
+      return next();
+    });
+  } else {
+    next();
+  }
+});
+
+server.beforeEach((req, res, next) => {
+  const routes = ["/", "/login", "/profile", "/new-post"];
+
+  if (routes.indexOf(req.url) !== -1 && req.method === "GET") {
+    return res.status(200).sendFile("public/index.html", "text/html");
+  }
+
+  next();
+});
 
 // ------ Files Routes ------ //
 
@@ -46,25 +103,54 @@ server.route("GET", "/api/posts", (req, res) => {
   res.status(200).json(posts);
 });
 
-// ------ Login Routes ------ //
-server.route("POST", "/api/login", (req, res) => {
-  let body = "";
-  req.on("data", (data) => {
-    body += data.toString("utf8");
-  });
+// Log user out
+server.route("DELETE", "/api/logout", (req, res) => {});
 
-  req.on("end", () => {
-    body = JSON.parse(body);
-    const { username, password } = body;
-    const user = USERS.find(
-      (user) => user.username === username && user.password === password
-    );
-    if (user && user.password === password) {
-      res.status(200).json({message: "Successfully logged in"});
-    } else {
-      res.status(401).json({ message: "Invalid username or password" });
-    }
-  });
+// Log user in and give them a token
+server.route("POST", "/api/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = USERS.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (user && user.password === password) {
+    const token = Math.floor(Math.random() * 10000000000).toString();
+    SESSIONS.push({ userId: user.id, token });
+
+    res.setHeader("Set-Cookie", `token=${token}; Path=/`);
+
+    res.status(200).json({ message: "Successfully logged in" });
+  } else {
+    res.status(401).json({ message: "Invalid username or password" });
+  }
+});
+
+// Update user info
+server.route("PUT", "/api/user", (req, res) => {});
+
+// Create new post
+server.route("POST", "/api/posts", (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+
+  console.log(req.userId);
+  const post = {
+    id: POSTS.length + 1,
+    title,
+    body,
+    userId: req.userId,
+  };
+
+  POSTS.push(post);
+  res.status(201).json(post);
+});
+
+// Send user info
+server.route("GET", "/api/user", (req, res) => {
+  const user = USERS.find((user) => user.id === req.userId);
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.json({ username: user.username, name: user.name });
 });
 
 // ------ Start Server ------ //
